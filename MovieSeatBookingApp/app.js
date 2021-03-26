231963221a1c2deb938e41b02ac3b387f961990d
Ankit <ankittrajj1996@gmail.com>
2021-03-26 23:16:45 +0530

    Project3 ---MovieSeat Booking App

---
+++ MovieSeatBookingApp/app.js
@@ -0,0 +1,6 @@
+const container = document.querySelector('.container');
+const seat = document.querySelectorAll('.row .seat:not(.occupied)');
+const count = document.getElementsByClassName('count');
+const price = document.getElementById('price');
+
+seat.add
